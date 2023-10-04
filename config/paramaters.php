505585a30b504f7e01e4ec699b5abc9eacc5538c
mochimochiowl <120357081+mochimochiowl@users.noreply.github.com>
2023-10-04 14:56:19 +0900

    add/定数をまとめるファイルを作成

---
+++ config/paramaters.php
@@ -0,0 +1,9 @@
+<?php
+// 文字数上限の設定
+define('EMAIL_CHAR_LIMIT', 200);
+define('NAME_CHAR_LIMIT', 15);
+define('LOGIN_ID_CHAR_LIMIT', 20);
+define('PASSWORD_CHAR_LIMIT', 20);
+
+// 初期値の設定
+define('BY_NAME_DEFAULT', 'デフォルト値');
