744dba82b7a1f1f30d7bde708405a9db44248e86
mochimochiowl <120357081+mochimochiowl@users.noreply.github.com>
2023-10-05 14:23:29 +0900

    update/User登録時にトランザクションを使用

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -28,42 +28,49 @@ class UserController extends Controller
     /** Userデータ（給与データや状態データを含む）の新規作成 */
     public function createUser(Request $request)
     {
-        // Userの作成
-        $user = User::query()->create(
-            [
-                'kanji_last_name' => $request['kanji_last_name'],
-                'kanji_first_name' => $request['kanji_first_name'],
-                'kana_last_name' => $request['kana_last_name'],
-                'kana_first_name' => $request['kana_first_name'],
-                'email' => $request['email'],
-                'login_id' => $request['login_id'],
-                'password' => Hash::make($request['password']),
-                'created_by' => '新規登録',
-                'updated_by' => '新規登録',
-            ]
-        );
 
-        Auth::login($user);
+        try {
+            return DB::transaction(function () use ($request) {
+                // Userの作成
+                $user = User::query()->create(
+                    [
+                        'kanji_last_name' => $request['kanji_last_name'],
+                        'kanji_first_name' => $request['kanji_first_name'],
+                        'kana_last_name' => $request['kana_last_name'],
+                        'kana_first_name' => $request['kana_first_name'],
+                        'email' => $request['email'],
+                        'login_id' => $request['login_id'],
+                        'password' => Hash::make($request['password']),
+                        'created_by' => '新規登録',
+                        'updated_by' => '新規登録',
+                    ]
+                );
 
-        $user_id = Auth::user()->user_id;
-        // UserSalaryの作成
-        $user->salary()->create([
-            'user_id' => $user_id,
-            'hourly_wage' => HOURLY_WAGE_DEFAULT,
-            'created_by' => '新規登録',
-            'updated_by' => '新規登録',
-        ]);
+                Auth::login($user);
 
-        // UserConditionの作成
-        $user->condition()->create([
-            'user_id' => $user_id,
-            'has_attended' => false,
-            'is_breaking' => false,
-            'created_by' => '新規登録',
-            'updated_by' => '新規登録',
-        ]);
+                $user_id = Auth::user()->user_id;
+                // UserSalaryの作成
+                $user->salary()->create([
+                    'user_id' => $user_id,
+                    'hourly_wage' => HOURLY_WAGE_DEFAULT,
+                    'created_by' => '新規登録',
+                    'updated_by' => '新規登録',
+                ]);
 
-        return redirect()->route('userInfo');
+                // UserConditionの作成
+                $user->condition()->create([
+                    'user_id' => $user_id,
+                    'has_attended' => false,
+                    'is_breaking' => false,
+                    'created_by' => '新規登録',
+                    'updated_by' => '新規登録',
+                ]);
+
+                return redirect()->route('userInfo');
+            }, 5);
+        } catch (\Exception $e) {
+            return redirect()->back()->with('error', '登録に失敗しました。');
+        }
     }
 
     public function login(Request $request)
