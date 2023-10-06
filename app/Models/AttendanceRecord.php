105e61da07eb8bd4edeceed2ee235ca0c81bb542
mochimochiowl <120357081+mochimochiowl@users.noreply.github.com>
2023-10-06 16:02:19 +0900

    update/primary_key設定、fillable追加

--- app/Models/AttendanceRecord.php
+++ app/Models/AttendanceRecord.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class AttendanceRecord extends Model
 {
     use HasFactory;
+
+    protected $primaryKey = 'at_record_id';
+    protected $fillable = [
+        'user_id',
+        'at_record_type',
+        'time',
+        'created_at',
+        'updated_at',
+        'created_by',
+        'updated_by',
+    ];
 }
