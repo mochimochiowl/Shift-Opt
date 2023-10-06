10715d7da7f2970d6ff8d017a26f90038023fb22
mochimochiowl <120357081+mochimochiowl@users.noreply.github.com>
2023-10-06 15:59:38 +0900

    add/at_record_type関係の定数を追加

--- config/parameters.php
+++ config/parameters.php
@@ -8,3 +8,14 @@ define('PASSWORD_CHAR_LIMIT', 20);
 // 初期値の設定
 define('BY_NAME_DEFAULT', 'デフォルト値');
 define('HOURLY_WAGE_DEFAULT', 0.);
+
+//at_record_typeの設定
+define('AT_RECORD_START_WORK', 'start_work');
+define('AT_RECORD_FINISH_WORK', 'finish_work');
+define('AT_RECORD_START_BREAK', 'start_break');
+define('AT_RECORD_FINISH_BREAK', 'finish_break');
+
+define('AT_RECORD_START_WORK_JP', '出勤');
+define('AT_RECORD_FINISH_WORK_JP', '退勤');
+define('AT_RECORD_START_BREAK_JP', '休憩始');
+define('AT_RECORD_FINISH_BREAK_JP', '休憩終');
