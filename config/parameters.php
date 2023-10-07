d0e4a685dd49eb47020fbfbd9913984490cd2082
mochimochiowl <120357081+mochimochiowl@users.noreply.github.com>
2023-10-07 16:50:05 +0900

    update/各テーブルのカラムの画面表示名を定数で定義

--- config/parameters.php
+++ config/parameters.php
@@ -19,3 +19,34 @@ define('AT_RECORD_START_WORK_JP', '出勤');
 define('AT_RECORD_FINISH_WORK_JP', '退勤');
 define('AT_RECORD_START_BREAK_JP', '休憩始');
 define('AT_RECORD_FINISH_BREAK_JP', '休憩終');
+
+//DB 共通のカラムの表示名を定義
+define('CREATED_BY_JP', '新規作成者');
+define('UPDATED_BY_JP', '最終更新者');
+define('CREATED_AT_JP', '新規作成日時');
+define('UPDATED_AT_JP', '最終更新日時');
+
+//DB userテーブルのカラムの表示名を定義
+define('USER_ID_JP', 'ユーザーID');
+define('KANJI_FIRST_NAME_JP', '姓（漢字）');
+define('KANJI_LAST_NAME_JP', '名（漢字）');
+define('KANA_FIRST_NAME_JP', '姓（漢字）');
+define('KANA_LAST_NAME_JP', '名（漢字）');
+define('EMAIL_JP', 'メールアドレス');
+define('EMAIL_VERIFIED_AT_JP', 'メールアドレス最終認証日時');
+define('LOGIN_ID_JP', 'ログインID');
+define('PASSWORD_JP', 'パスワード');
+
+//DB user_salariesテーブルのカラムの表示名を定義
+define('SALARY_ID_JP', '時給ID');
+define('HOURLY_WAGE_JP', '時給');
+
+//DB user_conditionsテーブルのカラムの表示名を定義
+define('CONDITION_ID_JP', 'コンディションID');
+define('HAS_ATTENDED_JP', AT_RECORD_START_WORK_JP . '打刻済みか');
+define('IS_BREAKING_JP', AT_RECORD_START_BREAK_JP . '打刻済みか');
+
+//DB attendance_recordsテーブルのカラムの表示名を定義
+define('AT_RECORD_ID_JP', 'レコードID');
+define('AT_RECORD_TYPE_JP', 'レコードタイプ');
+define('AT_RECORD_TIME_JP', '打刻時刻');
