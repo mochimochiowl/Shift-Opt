2ba9537a343d0371dd9f8ad235cf3b26876c1024
mochimochiowl <120357081+mochimochiowl@users.noreply.github.com>
2023-10-07 17:31:40 +0900

    update/主キーの項目名をuser_付きに変更

--- database/migrations/2023_10_05_135237_create_user_salaries.php
+++ database/migrations/2023_10_05_135237_create_user_salaries.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('user_salaries', function (Blueprint $table) {
-            $table->id('salary_id');
+            $table->id('user_salary_id');
             $table->foreignId('user_id')->constrained(
                 table: 'users',
                 column: 'user_id',
