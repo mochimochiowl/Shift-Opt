62a8873c83ac07aa3f25db93987c5a69f65bb99b
mochimochiowl <120357081+mochimochiowl@users.noreply.github.com>
2023-10-30 17:58:26 +0900

    update/メニュー内のボタン、ホバー演出の追加＋クリック領域を調整

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -13,23 +13,55 @@
         <ul id="menu" class="fixed top-0 left-0 w-full text-center text-white font-bold bg-blue-500 translate-x-full transition duration-300 ease-linear">
             @if (Auth::check())
                 <li class="h-24 p-9 bg-blue-600">{{Auth::user()->getKanjiFullName()}}さん、こんにちは！</li>
-            @endif
-            <li class="p-3"><a href="{{route('stamps.index')}}" class="">打刻</a></li>
-            @if (Auth::check())
-                @if (Auth::user()->is_admin)
-                    <li class="p-3"><a href="{{route('users.create')}}" class="">{{ConstParams::USER_JP}}登録</a></li>
-                    <li class="p-3"><a href="{{route('users.search')}}" class="">{{ConstParams::USER_JP}}検索</a></li>
-                    <li class="p-3"><a href="{{route('at_records.create')}}" class="">{{ConstParams::AT_RECORD_JP}}登録</a></li>
-                    <li class="p-3"><a href="{{route('at_records.search')}}" class="">{{ConstParams::AT_RECORD_JP}}検索</a></li>
-                    <li class="p-3"><a href="{{route('summary.index')}}" class="">日別実績集計表</a></li>
-                @endif
-            @else
+                @else
                 <li class="h-24 p-9 bg-blue-600">ログインしていません。</li>
             @endif
+            <li class="p-0 hover:text-gray-100 hover:bg-blue-400">
+                <a href="{{route('stamps.index')}}" class="block p-3">
+                    打刻
+                </a>
+            </li>
             @if (Auth::check())
-            <li class="p-3"><a href="{{route('logout')}}" class="">ログアウトする</a></li>
+                @if (Auth::user()->is_admin)
+                    <li class="p-0 hover:text-gray-100 hover:bg-blue-400">
+                        <a href="{{route('users.create')}}" class="block p-3">
+                            {{ConstParams::USER_JP}}登録
+                        </a>
+                    </li>
+                    <li class="p-0 hover:text-gray-100 hover:bg-blue-400">
+                        <a href="{{route('users.search')}}" class="block p-3">
+                            {{ConstParams::USER_JP}}検索
+                        </a>
+                    </li>
+                    <li class="p-0 hover:text-gray-100 hover:bg-blue-400">
+                        <a href="{{route('at_records.create')}}" class="block p-3">
+                            {{ConstParams::AT_RECORD_JP}}登録
+                        </a>
+                    </li>
+                    <li class="p-0 hover:text-gray-100 hover:bg-blue-400">
+                        <a href="{{route('at_records.search')}}" class="block p-3">
+                            {{ConstParams::AT_RECORD_JP}}検索
+                        </a>
+                    </li>
+                    <li class="p-0 hover:text-gray-100 hover:bg-blue-400">
+                        <a href="{{route('summary.index')}}" class="block p-3">
+                            日別実績集計表
+                        </a>
+                    </li>
+                @endif
+            @endif
+            @if (Auth::check())
+            <li class="p-0 hover:text-gray-100 hover:bg-blue-400">
+                <a href="{{route('logout')}}" class="block p-3">
+                    ログアウト
+                </a>
+            </li>
             @else
-            <li class="p-3"><a href="{{route('login.form')}}" class="">ログインする</a></li>
+            <li class="p-0 hover:text-gray-100 hover:bg-blue-400">
+                <a href="{{route('login.form')}}" class="block p-3">
+                    ログイン
+                </a>
+            </li>
             @endif
         </ul>
     </nav>
